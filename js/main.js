8c8da8985e08eacc6f5854f4b7da36c96d42a54a
webtimise <hello@webtimise.fr>
2021-01-24 14:44:06 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -95,8 +95,8 @@ function loadingScreenReveal() {
     tl.to(loadingScreen, { display: "none", autoAlpha: 0 }, "<.5");
 //	tl.to(bgRevealer, { xPercent: 101, ease: Expo.easeInOut, stagger: 0.08 }, "<.3");
 
-    tl.to(path, 0.8, {attr: { d: start }, stagger: 0.01}, "<");
-    tl.to(path, 0.4, {attr: { d: end }, stagger: 0.01}, "<");
+    tl.to(path, 0.8, {attr: { d: start }, stagger: 0.01});
+    tl.to(path, 0.4, {attr: { d: end }});
     /*
     tl.to(path, 0.8, {attr: { d: start }, ease: Power2.easeIn}, "<.5");
     tl.to(path, 0.4, {attr: { d: end }, ease: Power2.easeOut});
