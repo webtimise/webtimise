5a14e5d7076c2534f1f24795033bb99114b33f7c
webtimise <hello@webtimise.fr>
2021-05-13 21:17:21 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -28,8 +28,8 @@ function masterHeroReveal() {
 // Transition Screen
 function transitionScreenReveal() {
     const $path = select(".path");
-    const start = "M 0 100 V 50 Q 50 0 100 50 V 100 z";
-    const end =   "M 0 100 V 0 Q 50 0 100 0 V 100 z";
+    const end = "M 0 100 V 50 Q 50 0 100 50 V 100 z";
+    const start =   "M 0 100 V 0 Q 50 0 100 0 V 100 z";
     
     const tl_Transition = gsap.timeline();
         tl_Transition.to($path, 0.8, {attr: { d: start }, ease: Power2.easeIn});
