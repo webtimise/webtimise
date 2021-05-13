a29b1e04404fd4e1adf7fd72cb82e4ebee146029
webtimise <hello@webtimise.fr>
2021-05-14 00:04:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function masterHeroReveal() {
 function transitionScreenReveal() {
     const $path = select(".path");
     const start = "M 0 100 V 0 Q 50 0 100 0 V 100 z";
-    const end =   "M 0 100 V 100 Q 50 100 100 100 V 100 z";
+    const end =   "M 0 100 V 100 Q 50 90 100 100 V 100 z";
 	/*
     const start = "M 0 100 V 50 Q 50 0 100 50 V 100 z";
     const end =   "M 0 100 V 0 Q 50 0 100 0 V 100 z";
