4365cf8a2c13feb453a89f222326184897c4e110
webtimise <hello@webtimise.fr>
2021-05-14 00:13:05 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function transitionScreenReveal() {
     const $path = select(".path");
     
 	const start = "M 0 100 V 0 Q 50 0 100 0 V 100 z";
-    const end =   "M 0 100 V 100 Q 50 90 100 100 V 100 z";
+    const end =   "M 0 0 V 0 Q 50 10 100 0 V 0 z";
 	
 	/*
     const start = "M 0 100 V 50 Q 50 0 100 50 V 100 z";
